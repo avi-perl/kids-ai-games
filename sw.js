688445b32879da19290aa702d067870a1a2f93ae
Avi Perl <avi@aviperl.me>
2026-04-19 12:21:57 -0400

    github page

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE = 'fun-games-v1';
+
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon.svg',
+  './icons/icon-maskable.svg',
+  './hill-jumper/',
+  './hill-jumper/index.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Stale-while-revalidate: serve cache instantly, update in background
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  e.respondWith(
+    caches.open(CACHE).then(async cache => {
+      const cached = await cache.match(e.request);
+      const fetchPromise = fetch(e.request).then(res => {
+        if (res && res.status === 200) cache.put(e.request, res.clone());
+        return res;
+      }).catch(() => null);
+      return cached || fetchPromise;
+    })
+  );
+});
